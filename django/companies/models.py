782181d4b6c3d117b4130a8bc1a40e721dafa95b
José Victor Modesto <j.victormodesto75@gmail.com>
2025-12-13 11:30:50 -0300

    Modulo de autenticação

--- django/companies/models.py
+++ django/companies/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+# empresa
 class Enterprise(models.Model):
     name = models.CharField(max_length=180)
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+
+# funcionario
+class Employee(models.Model):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
